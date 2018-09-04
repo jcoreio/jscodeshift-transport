f6ac9ce140018171ec03c643584157a77d4d2556
Andy Edwards <jedwards@fastmail.com>
2018-09-04 01:11:32 -0500

    feat: initial commit

--- index.js
+++ index.js
@@ -1,2 +1,75 @@
-// @flow
+const j = require('jscodeshift')
+const path = require('path')
 
+module.exports = function replaceSourcesTransform(fileInfo, api, {find, replace}) {
+  if (!find) throw new Error('missing find option, pass --find=<VALUE>')
+  if (!replace) throw new Error('missing find option, pass --replace=<VALUE>')
+  const root = api.jscodeshift(fileInfo.source)
+  replaceSources(fileInfo.path, root, find, replace)
+  return root.toSource()
+}
+module.exports.parser = 'babylon'
+
+function findAbsoluteImports(root, source) {
+  return root.find(j.ImportDeclaration, {source: {value: source}})
+}
+
+const isTrueRequire = path => path.scope.getBindings().require == null
+
+function findAbsoluteRequires(root, source) {
+  return root.find(j.CallExpression, {
+    callee: {name: 'require'},
+    arguments: [{type: 'StringLiteral', value: source}],
+  }).filter(isTrueRequire)
+}
+
+function findRelativeImports(file, root, sourcePath) {
+  const absolutePath = path.resolve(sourcePath)
+  return root.find(j.ImportDeclaration, node =>
+    path.resolve(path.dirname(file), node.source.value) === absolutePath
+  )
+}
+
+function findRelativeRequires(file, root, sourcePath) {
+  const absolutePath = path.resolve(sourcePath)
+  return root.find(j.CallExpression, {
+    callee: {name: 'require'},
+    arguments: [{type: 'StringLiteral'}],
+  }).filter(nodePath => {
+    const {node: {arguments: [{value}]}} = nodePath
+    return isTrueRequire(nodePath) && path.resolve(path.dirname(file), value) === absolutePath
+  })
+}
+
+function replaceSources(file, root, find, replace) {
+  if (typeof replace === 'string') {
+    const target = replace.startsWith('.')
+      ? path.relative(path.dirname(file), path.resolve(replace)).replace(/^(?!\.)/, './')
+      : replace
+    replace = () => target
+  }
+
+  function processImport(nodePath) {
+    const {node} = nodePath
+    const {source: {value: source}} = node
+    const replacement = replace({file, path: nodePath, source})
+    if (typeof replacement === 'string') node.source.value = replacement
+  }
+
+  function processRequire(nodePath) {
+    const {node} = nodePath
+    const [{value: source}] = node.arguments
+    const replacement = replace({file, path: nodePath, source})
+    if (typeof replacement === 'string') node.arguments[0].value = replacement
+  }
+
+  if (find.startsWith('.')) {
+    findRelativeImports(file, root, find).forEach(processImport)
+    findRelativeRequires(file, root, find).forEach(processRequire)
+  } else {
+    findAbsoluteImports(root, find).forEach(processImport)
+    findAbsoluteRequires(root, find).forEach(processRequire)
+  }
+}
+
+module.exports.replaceSources = replaceSources
--- test/index.js
+++ test/index.js
@@ -1,10 +1,72 @@
-// @flow
+const {expect} = require('chai')
+const j = require('jscodeshift').withParser('babylon')
+const {replaceSources} = require('..')
+const {spawn} = require('promisify-child-process')
+const fs = require('fs-extra')
+const path = require('path')
 
-require('..')
+process.chdir(__dirname)
 
-describe('test setup', function () {
-  it('works', function () {
+const code = `
+import foo from 'foo'
+const foo = require('foo')
 
+import baz from './baz'
+const baz = require('./baz')
+
+import qux from '../qux'
+const qux = require('../qux')
+`
+
+const expected = `
+import foo from "./test/foo"
+const foo = require("./test/foo")
+
+import baz from "baz"
+const baz = require("baz")
+
+import qux from "../QUX"
+const qux = require("../QUX")
+`
+
+describe(`replaceSources`, function () {
+  it(`works`, function () {
+    const root = j(code)
+
+    const file = path.resolve(__dirname, '../temp.js')
+
+    replaceSources(file, root, 'foo', './foo')
+    replaceSources(file, root, '../baz', 'baz')
+    replaceSources(file, root, '../../qux', ({source}) => source.toUpperCase())
+
+    expect(root.toSource()).to.equal(expected)
   })
 })
+describe(`integration test`, async function () {
+  this.timeout(30000)
 
+  before(() => fs.writeFile('../temp.js', code, 'utf8'))
+  after(() => fs.remove('../temp.js'))
+  it(`works`, async function () {
+    await spawn('jscodeshift', [
+      '-t', '../index.js',
+      '../temp.js',
+      '--find=foo',
+      '--replace=./foo',
+    ], {stdio: 'inherit'})
+    await spawn('jscodeshift', [
+      '-t', '../index.js',
+      '../temp.js',
+      '--find=../baz',
+      '--replace=baz',
+    ], {stdio: 'inherit'})
+    await spawn('jscodeshift', [
+      '-t', '../index.js',
+      '../temp.js',
+      '--find=../../qux',
+      '--replace=../../QUX',
+    ], {stdio: 'inherit'})
+    const result = await fs.readFile('../temp.js', 'utf8')
+    expect(result).to.equal(expected)
+  })
+})
